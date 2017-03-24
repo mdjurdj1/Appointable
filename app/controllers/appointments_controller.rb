034b51abdd918d6bbe393ed555fcd0a192eda713
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-03-24 01:20:37 +0000

    Created appointment form partial

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -5,7 +5,26 @@ class AppointmentsController < ApplicationController
   end
 
   def new
-    @appointment = Appointment.new
+    @appointment = current_user.appointments.build
+  end
+
+  def create
+    @appointment = current_user.appointments.build(appointment_params)
+    if @appointment.save
+      redirect_to appointment_path(@appointment)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @appointment = Appointment.find_by(id: params[:id])
+  end
+
+
+  private
+  def appointment_params
+    params.require(:appointment).permit(:name, :description, :start_time)
   end
 
 end
