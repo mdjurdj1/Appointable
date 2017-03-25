2b6fa0586123e8c1828af76f48c4a659362f564c
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-03-25 20:24:45 +0000

    Converted ocation form to table for viewability

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,5 +3,9 @@ class LocationsController < ApplicationController
   def index 
     @locations = current_user.locations.all
   end 
+
+  def new 
+    @location = current_user.locations.build
+  end 
   
 end
