6c1d3c95afcb007a38e57fbb242dd6108c8c4e34
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-03-25 03:33:32 +0000

    Contact show now working, renders contact partial

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,22 @@
 class ContactsController < ApplicationController
+  before_action :set_contact, only: [:show, :edit, :update, :destroy]
 
   def index
     @contacts = current_user.contacts
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end 
+
+
+  private
+  def set_contact
+    @contact = Contact.find_by(id: params[:id])
   end
 end
