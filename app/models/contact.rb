e6fb374c152c9f450f10fd3c8b617c73ea7820a5
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-03-27 06:49:30 +0000

    Converted all navbar links to dropdowns

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,11 @@
 class Contact < ApplicationRecord
   validates :name, presence: true
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   validates :phone_number, format: { with: /\A\d{3}-\d{3}-\d{4}\z/ }
 
   belongs_to :user
   has_many :appointments
   has_many :locations, through: :appointments
-  
+
   scope :search, lambda {|search| where(["name LIKE ?", "%#{search}%"])}
 end
