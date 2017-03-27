d11ba297351eab5a010d2d4e4b1534c0037aa6e4
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-03-27 03:46:49 +0000

    Added relationships to contact model for expandability

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,5 +4,8 @@ class Contact < ApplicationRecord
   validates :phone_number, format: { with: /\A\d{3}-\d{3}-\d{4}\z/ }
 
   belongs_to :user
+  has_many :appointments
+  has_many :locations, through: :appointments
+  
   scope :search, lambda {|search| where(["name LIKE ?", "%#{search}%"])}
 end
