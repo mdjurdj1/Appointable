ff40166a4ddecd575ec0006cc2e8ad9d465557c8
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-04-21 10:03:28 +0000

    Index page is fully working now for appointments and their nested contacts

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -1,3 +1,59 @@
+////APPOINTMENT MODEL AND PROTOTYPE METHODS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+var Appointment = function(data) {
+  this.name = data.name
+  this.description = data.description
+  this.start_time = data.start_time
+  this.contact = {
+    id: data.contact.id,
+    name: data.contact.name,
+    email: data.contact.email,
+    phone_number: data.contact.phone_number
+  }
+  this.location = {
+    id: data.location.id,
+    name: data.location.name
+  }
+}
+
+Appointment.prototype.showContactFields = function() {
+  output = `
+  <p><strong>Contact</strong>: <a href="contacts/${this.contact.id}">${this.contact.name}</a>
+   - ${this.contact.email} - ${this.contact.phone_number}</p>`
+   return output
+}
+
+Appointment.prototype.showLocationFields = function() {
+  output = `
+  <p><strong>Location</strong>: <a href="locations/${this.location.id}">${this.location.name}</a></p>`
+   return output
+}
+
+Appointment.prototype.showLinkFields = function() {
+  output = `
+  <a href='/appointments/${this.id}/edit'>Edit</a> |
+  <a data-confirm="Are you sure?" data-method="delete" href="/appointments/${this.id}" rel="nofollow">Delete</a> |
+  <a href="/appointments">Back</a>`
+  return output
+}
+
+Appointment.prototype.showLegendHeader = function() {
+  output = `
+  <fieldset>
+    <legend>${this.name}</legend>
+  </fieldset>`
+  return output
+}
+
+Appointment.prototype.showInfoFields = function() {
+  output = `
+  <p><strong>Description</strong>: ${this.description}</p>
+  <p><strong>Start Time</strong>: ${this.start_time}</p>`
+  return output
+}
+
+//// LOADING LISTENERS AND LISTENER DEPENDENCIES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 var attachListeners = () => {
   $("#list_appointments").click(function(e) {
     e.preventDefault()
@@ -20,20 +76,15 @@ var listAppointments = () => {
   $(".notice").hide()
   $.get('/list', function(data) {
     $.each(data, function(index, value) {
+      let currentAppointment = new Appointment(value)
       var appt_div = `
       <div class="boxed">
-        <fieldset>
-          <legend>${value['name']}</legend>
-        </fieldset>
-        <p><strong>Description</strong>: ${value['description']}</p>
-        <p><strong>Start Time</strong>: ${value['start_time']}</p>
-        <p><strong>Contact</strong>: <a href="contacts/${value['contact']['id']}">${value['contact']['name']}</a>
-         - ${value['contact']['email']} - ${value['contact']['phone_number']}</p>
-        <p><strong>Location</strong>: <a href="locations/${value['location']['id']}">${value['location']['name']}</a></p>
+        ${currentAppointment.showLegendHeader()}
+        ${currentAppointment.showInfoFields()}
+        ${currentAppointment.showContactFields()}
+        ${currentAppointment.showLocationFields()}
         <fieldset><legend></legend></fieldset>
-        <a href='/appointments/${value['id']}/edit'>Edit</a> |
-        <a data-confirm="Are you sure?" data-method="delete" href="/appointments/${value['id']}" rel="nofollow">Delete</a> |
-        <a href="/appointments">Back</a>
+        ${currentAppointment.showLinkFields()}
         <br />
       </div>`
       $("#appointments_list").append(appt_div)
